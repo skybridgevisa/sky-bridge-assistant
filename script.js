d0a78d3fedb35ae44133fe43565c8ecab3442f6a
skybridgevisa <travel.bysameer@gmail.com>
2026-09-17 14:33:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const SKY_BRIDGE_CONFIG = {
   BUSINESS_EMAIL: "skybridge.migrationoverseas@gmail.com",
   COUNSELOR_NAME: "Zoya Siddiqui",
   COUNSELOR_PHONE: "+91 8796854108",
-  CONTACT_PERSON_NAME: "Shayan Malik",
-  CONTACT_PERSON_PHONE: "+91 8527841206",
+  HEAD_COUNSELOR_NAME: "Shayan Malik",
+  HEAD_COUNSELOR_PHONE: "+91 8527841206",
   INTERNATIONAL_CONTACT_NAME: "Cristian Dobrea",
   INTERNATIONAL_CONTACT_PHONE: "+380 63 053 7559",
   DELHI_BRANCH_ADDRESS: "15A, 3rd Floor, Pocket 1, Near HDFC Bank, Mayur Vihar, Delhi – 110091, India"
